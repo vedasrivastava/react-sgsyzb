f46bc9d8922687ccdbfd7984e13ac9cc724e3503
vedasrivastava <120018854+vedasrivastava@users.noreply.github.com>
2023-06-16 06:24:14 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './style.css';
 import Resume from './components/Resume';
+import resumeData from './data';
 
 export default function App() {
-  const resumeData = {
-    experience:[{year:2021, company:"cognizant", role:"Developer"}],
-    education:[{Graduation:"B.Tech",college:"DIT University" ,intermediate:"Pinewood School" }],
-    skill:["react js", "javascript","Git/GitHub"]
-  }
-
-
   return (
     <>
       <Resume title={'Resume'} data={resumeData}></Resume>
---
+++ src/data.js
@@ -0,0 +1,7 @@
+const resumeData = {
+  experience:[{year:2021, company:"cognizant", role:"Developer"}],
+  education:[{Graduation:"B.Tech",college:"DIT University" ,intermediate:"Pinewood School" }],
+  skill:["react js", "javascript","Git/GitHub"]
+}
+
+export default resumeData;
